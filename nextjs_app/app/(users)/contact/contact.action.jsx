eac317bcc808f3076ec3a9443a6d6e01f05a9a82
Het <hra.opash@gmail.com>
2026-03-12 11:40:05 +0530

    redirect function

--- nextjs_app/app/(users)/contact/contact.action.jsx
+++ nextjs_app/app/(users)/contact/contact.action.jsx
@@ -1,15 +1,18 @@
 "use server";
+import { redirect } from "next/navigation";
 import connectDB from "../../../config/database";
 import Contact from "../../../config/models/Contact";
 
 export const contactAction = async (fullName, email, message) => {
-//   const { fullName, email, message } = Object.fromEntries(formData.entries());
+  //   const { fullName, email, message } = Object.fromEntries(formData.entries());
 
   try {
     await connectDB();
     await Contact.create({ fullName, email, message });
-    return { success: true, message: "Contact saved successfully" };
+    // return { success: true, message: "Contact saved successfully" };
+    redirect("/");
   } catch (error) {
+    if (error.message === "NEXT_REDIRECT") throw error;
     return { success: false, message: "Failed to save contact" };
   }
 };
