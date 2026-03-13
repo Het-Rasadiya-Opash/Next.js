7823ea630a0be028848b6bd0ddfe772de2f8f81d
Het <hra.opash@gmail.com>
2026-03-13 13:23:38 +0530

    drawer box add

--- nextjs_app/app/(users)/page.js
+++ nextjs_app/app/(users)/page.js
@@ -1,11 +1,13 @@
 import { DialogDemo } from "@/components/DialogDemo";
+import DrawerDemo from "@/components/Drawer";
 
 export default async function Home() {
     return(
         <div>
             <h1>Home Page</h1>
             <section>
-                <DialogDemo/>
+                {/* <DialogDemo/> */}
+                <DrawerDemo/>
             </section>
         </div>
     )
